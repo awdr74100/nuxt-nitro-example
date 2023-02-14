729a027c326546da65d4b615e1a4769af969b48c
Yi-Ru Lan <a78945612385238@gmail.com>
2023-02-14 18:17:49 +0800

    Update progress

--- server/utils/token.ts
+++ server/utils/token.ts
@@ -1,24 +1,31 @@
 import { createSigner, createVerifier } from 'fast-jwt';
 import ms, { type StringValue } from 'ms';
 
-type Type = 'accessToken' | 'refreshToken';
+type TokenType = 'accessToken' | 'refreshToken';
 
-interface Payload {
+interface TokenPayload {
   [key: string]: string | number | boolean;
 }
 
-export const signToken = (type: Type, payload: Payload, exp: StringValue) => {
+export const signToken = (
+  type: TokenType,
+  payload: TokenPayload,
+  expiresIn: StringValue,
+) => {
   const config = useRuntimeConfig();
 
   const signSync = createSigner({
     key: config[`${type}Secret`],
-    expiresIn: ms(exp),
+    expiresIn: ms(expiresIn),
   });
 
   return signSync(payload);
 };
 
-export const verifyToken = <T extends Payload>(type: Type, token: string) => {
+export const verifyToken = <T extends TokenPayload>(
+  type: TokenType,
+  token: string,
+) => {
   const config = useRuntimeConfig();
 
   const verifySync = createVerifier({
