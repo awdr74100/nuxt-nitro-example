e25dc5dd7a2de6fe76105b27f371793bc6223693
Yi-Ru Lan <a78945612385238@gmail.com>
2023-02-17 00:17:59 +0800

    Update progress

--- server/utils/auth.ts
+++
@@ -1,8 +0,0 @@
-import type { H3Event } from 'h3';
-
-export const useAuth = <T>(event: H3Event) => {
-  const accessToken = getCookie(event, 'accessToken');
-
-  const payload = verifyAccessToken(accessToken || '');
-};
-U
